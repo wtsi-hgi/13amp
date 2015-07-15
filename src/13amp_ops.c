226314c55387c7e46fbdaf466d73321255f87cfd
Christopher Harrison <ch12@sanger.ac.uk>
2015-07-15 16:35:22 +0100

    FS operations, based on fusexmp.c

--- src/13amp_ops.c
+++ src/13amp_ops.c
@@ -6,6 +6,11 @@
 #include <errno.h>
 #include <string.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dirent.h>
+
 #include <fuse.h>
 
 /**
@@ -24,7 +29,13 @@ void* cramp_init(struct fuse_conn_info* conn) {
   @return  Exit status (0 = OK; -errno = not so much)
 */
 int cramp_getattr(const char* path, struct stat* stbuf) {
-  /* TODO */
+  int res = lstat(path, stbuf);
+
+  if (res == -1) {
+    return -errno;
+  }
+
+  return 0;
 }
 
 /**
@@ -34,7 +45,14 @@ int cramp_getattr(const char* path, struct stat* stbuf) {
   @return  Exit status (0 = OK; -errno = not so much)
 */
 int cramp_open(const char* path, struct fuse_file_info* fi) {
-  /* TODO */
+  int res = open(path, fi->flags);
+
+  if (res == -1) {
+    return -errno;
+  }
+
+  close(res);
+  return 0;
 }
 
 /**
@@ -47,7 +65,22 @@ int cramp_open(const char* path, struct fuse_file_info* fi) {
   @return  Exit status (0 = OK; -errno = not so much)
 */
 int cramp_read(const char* path, char* buf, size_t size, off_t offset, struct fuse_file_info* fi) {
-  /* TODO */
+  int fd, res;
+
+  (void)fi;
+
+  fd = open(path, O_RDONLY);
+  if (fd == -1) {
+    return -errno;
+  }
+
+  res = pread(fd, bug, size, offset);
+  if (res == -1) {
+    return -errno;
+  }
+
+  close(fd);
+  return res;
 }
 
 /**
@@ -60,7 +93,31 @@ int cramp_read(const char* path, char* buf, size_t size, off_t offset, struct fu
   @return  Exit status (0 = OK; -errno = not so much)
 */
 int cramp_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
-  /* TODO */
+  DIR* dp;
+  struct dirent* de;
+
+  (void)offset;
+  (void)fi;
+
+  dp = opendir(path);
+  if (dp == NULL) {
+    return -errno;
+  }
+
+  while ((de = readdir(dp)) != NULL) {
+    struct stat st;
+    memset(&st, 0, sizeof(st));
+    
+    st.st_ino = de->d_ino;
+    st.st_mode = de->d_type << 12;
+
+    if (filler(buf, de->d_name, &st, 0)) {
+      break;
+    }
+  }
+
+  closedir(dp);
+  return 0;
 }
 
 /**
@@ -70,7 +127,10 @@ int cramp_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t off
   @return  Exit status (0 = OK; -errno = not so much)
 */
 int cramp_release(const char* path, struct fuse_file_info* fi) {
-  /* TODO */
+  /* XXX Does this need to be implemented? */
+  (void)path;
+  (void)fi;
+  return 0;
 }
 
 /**
